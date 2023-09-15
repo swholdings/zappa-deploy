6fa2cf2d8c134f16cb4f84484e0686698bdf9667
Andrea Ciantelli <andrea.ciantelli@sportwig.com>
2023-09-15 13:30:55 +0200

    removed region from aws config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 cd $GITHUB_WORKSPACE/.
 mkdir -p ~/.aws
-echo "[sportwig]" > ~/.aws/config
-echo "region=$1" >> ~/.aws/config
+# echo "[sportwig]" > ~/.aws/config
+# echo "region=$1" >> ~/.aws/config
 echo "[sportwig]" > ~/.aws/credentials
 echo "aws_access_key_id=$2" >> ~/.aws/credentials
 echo "aws_secret_access_key=$3" >> ~/.aws/credentials
@@ -12,7 +12,6 @@ echo "aws_secret_access_key=$3" >> ~/.aws/credentials
 virtualenv .venv
 source ./.venv/bin/activate
 
-python -m pip install pip -U
 python -m pip install -r requirements/deploy.txt
 
 {
